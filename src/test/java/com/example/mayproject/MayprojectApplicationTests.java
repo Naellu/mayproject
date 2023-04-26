d681c818552011b3967456d2d1251c260b0239cf
Naellu <119831581+Naellu@users.noreply.github.com>
2023-04-26 09:23:46 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/mayproject/MayprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
